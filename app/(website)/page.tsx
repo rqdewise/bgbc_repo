5b73fd6954a107da6643fc62c13ba77eafa46077
rqdeleon <135800668+rqdewise@users.noreply.github.com>
2024-07-26 23:40:09 +0800

    Update page.tsx

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,7 +1,7 @@
 import WidgetServices from "@/components/widgetServices"
-import WidgetHeroBanner from "@/components/WidgetHeroBanner"
+import WidgetHeroBanner from "@/components/widgetHeroBanner"
 import { InfoTabs } from "@/components/info-tabs";
-import WidgetContact from "@/components/WidgetContact"
+import WidgetContact from "@/components/widgetContact"
 import WidgetPastorBanner from "@/components/widgetPastorBanner"
 
 export default function Home() {
